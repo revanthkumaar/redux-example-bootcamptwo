93c25464e58b61cb6a935a8442bef318c2fb5b66
revanthkumaar <iamrevanthkumar@gmail.com>
2021-09-22 18:31:33 +0530

    asldjkhl
    qqkjl;asjd

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { configure } from "@testing-library/react";
+import {createStore} from "redux";
+import rotateReducer from "./reducers/rotateReducer"
+
+function configureStore(state={rotating:true}) {
+    return createStore(rotateReducer,state)
+}
+
+export default configureStore;
